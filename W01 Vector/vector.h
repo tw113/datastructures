5a8e0f0892a58b65f7726672581d61909e99b1b6
tw113 <tdub113@gmail.com>
2019-09-23 14:56:16 -0600

    stubbin

--- W01 Vector/vector.h
+++ W01 Vector/vector.h
@@ -1,1 +1,50 @@
-//comments
+namespace custom {
+
+    template <class T>
+    class vector {
+
+        class iterator {
+            private:
+                T pointer = NULL;
+
+            public:
+                iterator();
+                iterator(T p);
+                iterator(iterator rhs);
+                
+                iterator assignment(iterator it);
+                bool isEquals(iterator it);
+                bool isNEqual(iterator it);
+                iterator increment();
+                iterator decrement();
+                T dereference();
+        }; 
+
+        private:
+            T* array = NULL;
+
+        public:
+            int numElements;
+            int numCapacity;
+
+            vector();
+            vector(int numElements);
+            vector(int numElements, T t);
+            vector(vector rhs);
+
+            vector assign(vector rhs);
+            int size();
+            int capacity();
+            bool empty();
+            clear();
+            buffer();
+
+            push_back(T t);
+            T access(int index);
+            iterator begin();
+            iterator end();
+
+            resize(int numCapacity);
+    };
+
+}
