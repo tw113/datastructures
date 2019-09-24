df9ec4745d89b8eb6ef28c80b210d714a554e740
cdubspence <orangobang@gmail.com>
2019-09-24 13:21:48 -0600

    cpp file

---
+++ W01 Vector/vector.cpp
@@ -0,0 +1,32 @@
+#include "vector.h"
+
+using namespace custom;
+
+template <class T>
+bool vector <T> :: empty() {
+    if(numElements == 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+template <class T>
+void vector <T> :: clear() {
+    numElements = 0;
+}
+
+template <class T>
+void vector <T> :: push_back(T t){
+    if(size() == capacity()) {
+        resize(capacity() + 1);
+    }
+
+}
+template <class T>
+int vector <T> :: size(){
+    for(int i = 0; i < numElements; i++){
+        cout << numElements << endl;
+    }
+}
